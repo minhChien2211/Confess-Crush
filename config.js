b662b99858728a6f7c402d23a3b15343e7b86ffd
minhChien2211 <106471710+minhChien2211@users.noreply.github.com>
2022-05-29 18:02:45 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
     titleWeb: "Demo",
-    introTitle: 'Nguyễn Thị ......',
+    introTitle: 'Vương Hoài',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
